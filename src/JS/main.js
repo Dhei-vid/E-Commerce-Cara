1e126fead6f83020f96bf496da237560c1ca893a
Dhei-vid <david.etuk@aun.edu.ng>
2022-07-23 00:57:05 +0100

    Update 1

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,6 +1,33 @@
-const mobileBtn = document.querySelector(".mobileMenu");
-const navEle = document.getElementById("navbar");
+// Hot module Replacement
+if (module.hot) {
+  module.hot.accept();
+}
 
-mobileBtn.addEventListener("click", function () {
+const navEle = document.getElementById("navbar");
+const mobileMenu = document.querySelector("#mobileMenu");
+const mobileClose = document.getElementById("mobileClose");
+
+mobileMenu.addEventListener("click", function () {
   navEle.classList.add("active");
 });
+
+mobileClose.addEventListener("click", function () {
+  navEle.classList.remove("active");
+});
+
+function fizzBuzz(n) {
+  // Write your code here
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 == 0 && i % 5 != 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0 && i % 3 != 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz(15);
