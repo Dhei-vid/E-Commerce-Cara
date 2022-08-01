fa9bd11e495d0c45ac4b0d20d3b9bf85d3bee382
Dhei-vid <david.etuk@aun.edu.ng>
2022-08-01 15:18:40 +0100

    new shopping page added

--- src/JS/main.js
+++ src/JS/main.js
@@ -14,20 +14,3 @@ mobileMenu.addEventListener("click", function () {
 mobileClose.addEventListener("click", function () {
   navEle.classList.remove("active");
 });
-
-function fizzBuzz(n) {
-  // Write your code here
-  for (let i = 1; i <= n; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 == 0 && i % 5 != 0) {
-      console.log("Fizz");
-    } else if (i % 5 == 0 && i % 3 != 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
-    }
-  }
-}
-
-fizzBuzz(15);
